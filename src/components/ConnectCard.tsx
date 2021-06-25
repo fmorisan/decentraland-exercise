030c0e96b9d3b9d1d484228132cc8770e2cd798e
Felipe Buiras <f.morisan@gmail.com>
2021-06-25 16:19:09 -0300

    feat: disable connect button when waiting for metamask

--- src/components/ConnectCard.tsx
+++ src/components/ConnectCard.tsx
@@ -5,13 +5,20 @@ import { useWeb3React } from '@web3-react/core'
 import MetamaskConnector from '../connectors'
 
 export default function ConnectCard() {
+    const [isWaiting, setIsWaiting] = React.useState<boolean>(false)
     const { activate } = useWeb3React()
 
     return (
         <Center>
-            <Button primary onClick={() => activate(MetamaskConnector, () => {
-                alert('Please connect to any supported network: Rinkeby or Localhost (1337)')
-            })}>
+            <Button primary disabled={isWaiting} onClick={() => {
+                setIsWaiting(true)
+                activate(MetamaskConnector, () => {
+                    alert('Please connect to any supported network: Rinkeby or Localhost (1337)')
+                }).finally(() => {
+                    setIsWaiting(false)
+                })
+            }
+            }>
                 Connect
             </Button>
         </Center>
