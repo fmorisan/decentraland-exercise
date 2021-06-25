af8a3d88aef617ffaf23e42c025a3133c938380b
Felipe Buiras <f.morisan@gmail.com>
2021-06-25 16:14:24 -0300

    fix: show form errors only when fields were modified

--- src/components/TransferForm.tsx
+++ src/components/TransferForm.tsx
@@ -11,24 +11,29 @@ import { useWeb3React } from '@web3-react/core'
 import { useAppDispatch, useAppSelector } from 'hooks'
 import { decrementBalanceByAmount } from 'features/balance'
 
+const INITIAL_RECEIVER = ""
+const INITIAL_AMOUNT = 0
+
 export default function TransferForm({contract}: {contract: Contract}) {
     const balance = useAppSelector((store) => store.balance.value)
     const dispatch = useAppDispatch()
 
     const { account, library } = useWeb3React()
 
-    const [ receiver, setReceiver ] = useState<string>("")
+    const [ receiver, setReceiver ] = useState<string>(INITIAL_RECEIVER)
     // We can just use a bare BigNumber since the Dummy token provided 
     // for the demo has no decimals.
     // If we were in a real setting, displaying this number would entail
     // dividing this number by 10**decimals whenever it's to be shown.
-    const [ amount, setAmount ] = useState<number>(0)
+    const [ amount, setAmount ] = useState<number>(INITIAL_AMOUNT)
     const [ waitingForTx, setWaiting ] = useState<boolean>(false)
-
 
     const isReceiverValid = isAddress(receiver)
     const isAmountValid = amount > 0 && amount <= balance
     const isFormValid = isReceiverValid && isAmountValid
+
+    const shouldShowReceiverError = !isReceiverValid && receiver !== INITIAL_RECEIVER
+    const shouldShowAmountError = !isAmountValid && amount !== INITIAL_AMOUNT
 
     function handleSubmit() {
         if (!isFormValid || !contract)
@@ -62,8 +67,8 @@ export default function TransferForm({contract}: {contract: Contract}) {
                     onChange={(evt) => {
                         setReceiver(evt.target.value || "")
                     }}
-                    error={!isReceiverValid}
-                    message={isReceiverValid?"":"Not a valid Ethereum address"}/>
+                    error={shouldShowReceiverError}
+                    message={shouldShowReceiverError?"Not a valid Ethereum address":""}/>
 
             <Field 
                     label="Amount"
@@ -72,8 +77,8 @@ export default function TransferForm({contract}: {contract: Contract}) {
                     onChange={(evt) => {
                         setAmount(parseInt(evt.target.value) || 0)
                     }}
-                    error={!isAmountValid}
-                    message={isAmountValid?"":`Amount should be between 0 and ${balance}`}/>
+                    error={shouldShowAmountError}
+                    message={shouldShowAmountError?`Amount should be between 0 and ${balance}`:""}/>
             
                 <Button
                         primary
